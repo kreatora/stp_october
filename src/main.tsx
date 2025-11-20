5fe95a5a1dacb2dc2a2c0e5e068105078b4ca89b
kreatora <dogukangungordu@gmail.com>
2025-11-20 15:34:53 +0100

    logo change

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,6 @@ const container = document.getElementById('root');
 if (container) {
   // For now, just render a simple message since this page doesn't seem to need React components
   ReactDOM.createRoot(container).render(
-    <React.StrictMode>
-      <div>Welcome to the Climate Policy Atlas</div>
-    </React.StrictMode>
+    <React.StrictMode></React.StrictMode>
   );
 }
